149884b6de5f638aa0db2284ab834a5feb3c8d69
Mariah <mariahschock@gmail.com>
2022-09-29 12:43:27 -0700

    TextAreaControl test is passing

--- src/components/FormControls/FormControl.test.jsx
+++ src/components/FormControls/FormControl.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { InputControl } from './FormControl';
+import { InputControl, TextAreaControl } from './FormControl';
 
 test('Input Control', async () => {
   render(
@@ -16,3 +16,21 @@ test('Input Control', async () => {
   expect(inputControl.placeholder).toBe('your name');
   expect(inputControl.required).toBe(true);
 });
+
+test('Text Area Control', async () => {
+  render(
+    <TextAreaControl
+      label="Who are you?"
+      name="bio"
+      required
+      placeholder="tell us more"
+    />
+  );
+
+  const textAreaControl = screen.getByLabelText('Who are you?');
+  expect(textAreaControl.name).toBe('bio');
+  expect(textAreaControl.required).toBe(true);
+  expect(textAreaControl.placeholder).toBe('tell us more');
+});
+
+
